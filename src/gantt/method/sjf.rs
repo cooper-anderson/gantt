e38e68c0678f073869a3522d16b51bc1a5e6e9b6
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-04-04 09:39:19 -0400

    Add basic sjf tests

--- src/gantt/method/sjf.rs
+++ src/gantt/method/sjf.rs
@@ -12,5 +12,25 @@ mod example {
 	fn order() {
 		let problem = Problem::example();
 		let answer = super::run(problem);
+		let order = vec![
+			"A", "A", "A", "B", "B", "B", "B", "B", "B", "E",
+			"E", "C", "C", "C", "C", "D", "D", "D", "D", "D"
+		];
+
+		assert_eq!(order, answer.order);
+	}
+
+	#[test]
+	fn turnaround() {
+		let problem = Problem::example();
+		let answer = super::run(problem);
+		assert_eq!(7.6, answer.turnaround);
+	}
+
+	#[test]
+	fn response() {
+		let problem = Problem::example();
+		let answer = super::run(problem);
+		assert_eq!(3.6, answer.turnaround);
 	}
 }

d5f2e1d2806d492f796b3f2b446c7e1a4e6dbd01
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-04-04 09:28:19 -0400

    Add basic fifo tests

--- src/gantt/method/fifo.rs
+++ src/gantt/method/fifo.rs
@@ -13,5 +13,25 @@ mod example {
 	fn order() {
 		let problem = Problem::example();
 		let answer = super::run(problem);
+		let order = vec![
+			"A", "A", "A", "B", "B", "B", "B", "B", "B", "C",
+			"C", "C", "C", "D", "D", "D", "D", "D", "E", "E"
+		];
+
+		assert_eq!(order, answer.order);
+	}
+
+	#[test]
+	fn turnaround() {
+		let problem = Problem::example();
+		let answer = super::run(problem);
+		assert_eq!(8.6, answer.turnaround);
+	}
+
+	#[test]
+	fn response() {
+		let problem = Problem::example();
+		let answer = super::run(problem);
+		assert_eq!(4.6, answer.turnaround);
 	}
 }

eca9c4378b1a412f6ce984bd0a61778c516bfc6e
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-04-05 04:49:42 -0400

    Temporarily disable stcf

--- src/gantt/method/stcf.rs
+++ src/gantt/method/stcf.rs
@@ -1,16 +1,16 @@
-use crate::gantt::{Answer, Problem};
+// use crate::gantt::{Answer, Problem};
 
-pub fn run(problem: Problem) -> Answer {
-	Answer::new("stcf", problem, vec![])
-}
+// pub fn run(problem: Problem) -> Answer {
+// 	Answer::new("stcf", problem, vec![])
+// }
 
-#[cfg(test)]
-mod example {
-	use crate::gantt::Problem;
+// #[cfg(test)]
+// mod example {
+// 	use crate::gantt::Problem;
 
-	#[test]
-	fn order() {
-		let problem = Problem::example();
-		let answer = super::run(problem);
-	}
-}
+// 	#[test]
+// 	fn order() {
+// 		let problem = Problem::example();
+// 		let answer = super::run(problem);
+// 	}
+// }

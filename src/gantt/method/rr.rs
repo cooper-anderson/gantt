8e8fa4a31d37bdc47413b8f276f7c3039d92edc9
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-04-05 05:27:16 -0400

    Implement rr

--- src/gantt/method/rr.rs
+++ src/gantt/method/rr.rs
@@ -1,7 +1,32 @@
-use crate::gantt::{Answer, Problem};
+use std::collections::{HashMap, VecDeque};
 
-pub fn run(problem: &Problem, q: i32) -> Answer {
-	Answer::new(format!("rr q={}", q).as_str(), problem, vec![])
+use crate::gantt::{Answer, Problem, Process};
+
+pub fn run(problem: &Problem, q: usize) -> Answer {
+	let mut order: Vec<String> = Vec::new();
+	let mut queue: VecDeque<&Process> = VecDeque::new();
+	let mut counts: HashMap<&String, usize> = HashMap::new();
+
+	for i in 0..problem.length {
+		for proc in &problem.processes {
+			if i == proc.arrival { queue.push_back(proc); }
+		}
+
+		match queue.pop_front() {
+			Some(p) => {
+				let count = counts.entry(&p.label).or_insert(0);
+				order.push(p.label.clone());
+				*count += 1;
+				if *count != p.service {
+					if *count % q == 0 { queue.push_back(p) }
+					else { queue.push_front(p) }
+				}
+			}
+			None => {}
+		}
+	}
+
+	Answer::new(format!("rr q={}", q).as_str(), &problem, order)
 }
 
 #[cfg(test)]
@@ -42,7 +67,7 @@ mod example_q1 {
 	#[test]
 	fn order() {
 		let problem = Problem::example();
-		let answer = super::run(&problem, 4);
+		let answer = super::run(&problem, 1);
 		let order = vec![
 			"A", "A", "B", "A", "B", "C", "B", "D", "C", "B",
 			"E", "D", "C", "B", "E", "D", "C", "B", "D", "D"
@@ -54,14 +79,14 @@ mod example_q1 {
 	#[test]
 	fn turnaround() {
 		let problem = Problem::example();
-		let answer = super::run(&problem, 4);
+		let answer = super::run(&problem, 1);
 		assert_eq!(10.8, answer.turnaround);
 	}
 
 	#[test]
 	fn response() {
 		let problem = Problem::example();
-		let answer = super::run(&problem, 4);
+		let answer = super::run(&problem, 1);
 		assert_eq!(0.8, answer.response);
 	}
 }

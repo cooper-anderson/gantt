50f11557e4bce6beea38e83d2746281c6716fdd0
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-04-05 04:48:08 -0400

    Add methods for getting turnaround and response

--- src/gantt/mod.rs
+++ src/gantt/mod.rs
@@ -3,25 +3,25 @@ pub mod method;
 #[derive(PartialEq, Eq, Hash)]
 pub struct Process {
 	pub label: String,
-	pub arrival: i32,
-	pub service: i32
+	pub arrival: usize,
+	pub service: usize
 }
 
 impl Process {
-	fn new(raw: (&str, i32, i32)) -> Self {
+	fn new(raw: (&str, usize, usize)) -> Self {
 		Process { label: raw.0.to_string(), arrival: raw.1, service: raw.2 }
 	}
 }
 
 pub struct Problem {
 	pub processes: Vec<Process>,
-	pub length: i32
+	pub length: usize
 }
 
 impl Problem {
-	pub fn new(raw: Vec<(&str, i32, i32)>) -> Self {
+	pub fn new(raw: Vec<(&str, usize, usize)>) -> Self {
 		let mut processes: Vec<Process> = Vec::new();
-		let mut length: i32 = 0;
+		let mut length: usize = 0;
 
 		for p in raw {
 			length += p.2;
@@ -50,9 +50,30 @@ pub struct Answer {
 }
 
 impl Answer {
-	pub fn new(
-		name: &str, order: Vec<String>, turnaround: f32, response: f32
-	) -> Self {
+	pub fn new(name: &str, problem: Problem, order: Vec<String>) -> Self {
+		let mut turnaround: f32 = 0.0;
+		let mut response: f32 = 0.0;
+		let size = problem.processes.len() as f32;
+		for process in problem.processes {
+			turnaround += Answer::get_turnaround(&order, &process) as f32;
+			response += Answer::get_response(&order, &process) as f32;
+		}
+		turnaround /= size;
+		response /= size;
 		Answer {name: name.to_string(), order, turnaround, response}
+	}
+
+	fn get_turnaround(order: &Vec<String>, process: &Process) -> usize {
+		match order.iter().rposition(|s| s.as_str() == process.label) {
+			Some(i) => i - process.arrival + 1,
+			None => 0
+		}
+	}
+
+	fn get_response(order: &Vec<String>, process: &Process) -> usize {
+		match order.iter().position(|s| s.as_str() == process.label) {
+			Some(i) => i - process.arrival,
+			None => 0
+		}
 	}
 }
